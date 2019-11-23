7df27d2957b4568f4fd82f286efb6b775c6af349
Adam Romański <adam.romanski@netguru.pl>
2019-11-23 22:27:57 +0100

    02.3

---
+++ 02/hatta-website/.cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/adamromanski/eduweb/KURS – Gatsby/code/02/hatta-website/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/adamromanski/eduweb/KURS – Gatsby/code/02/hatta-website/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/adamromanski/eduweb/KURS – Gatsby/code/02/hatta-website/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/adamromanski/eduweb/KURS – Gatsby/code/02/hatta-website/src/pages/page-2.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/adamromanski/eduweb/KURS – Gatsby/code/02/hatta-website/src/pages/blog.js")))
+}
+
