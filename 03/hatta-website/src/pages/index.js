dee7fdfa9c807a759d618a6c0c345dd8e0eb9543
Adam Romański <adam.romanski@netguru.pl>
2019-11-24 15:50:17 +0100

    03

---
+++ 03/hatta-website/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components';
+import Navigation from "../components/Navigation/Navigation";
+
+const IndexPage = () => (
+  <>
+    <h1>Hello Hatta</h1>
+  </>
+)
+
+export default IndexPage
