c85c44faed4682d8cf14cb2675b04533a3aa96f3
Adam Romański <adam.romanski@netguru.pl>
2019-12-22 23:34:33 +0100

    04.2-finish

--- 04/hatta-website/src/pages/index.js
+++ 04/hatta-website/src/pages/index.js
@@ -1,24 +1,59 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
 import styled from 'styled-components';
-import Navigation from "../components/Navigation/Navigation";
-import Button from "../components/Button/Button";
+import Button from '../components/Button/Button';
 
-const IndexPage = ({data}) => (
+const ContentWrapper = styled.div`
+  width: 60%;
+  height: calc(100vh - 80px);
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+
+  h1 {
+    font-size: 105px;
+    margin: 0;
+    width: 60%;
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 60px 0 40px;
+    width: 40%;
+  }
+`;
+
+const ImageWrapper = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 40%;
+  height: 100vh;
+  object-fit: cover;
+`;
+
+const IndexPage = ({ data }) => (
   <>
-    <h1>Hello Hatta</h1>
-    <p>While artists work from real to the abstract, architects must work from the abstract to the real.</p>
-    <Button>estimate project</Button>
-    <img src={data.file.publicURL} />
+    <ContentWrapper>
+      <h1>Your new space</h1>
+      <p>
+        While artists work from real to the abstract, architects must work from
+        the abstract to the real.
+      </p>
+      <Button>estimate project</Button>
+    </ContentWrapper>
+    <ImageWrapper src={data.file.publicURL} />
   </>
-)
+);
 
 export const query = graphql`
   {
-    file(name: {eq: "hero"}) {
+    file(name: { eq: "hero" }) {
       publicURL
     }
   }
-`
+`;
 
-export default IndexPage
+export default IndexPage;
