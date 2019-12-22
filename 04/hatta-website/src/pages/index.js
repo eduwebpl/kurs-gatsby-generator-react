3db0123b23134202a3c64b56445ab19bca3862f5
Adam Romański <adam.romanski@netguru.pl>
2019-12-22 21:31:31 +0100

    04

---
+++ 04/hatta-website/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components';
+import Navigation from "../components/Navigation/Navigation";
+import Button from "../components/Button/Button";
+
+const IndexPage = () => (
+  <>
+    <h1>Hello Hatta</h1>
+    <p>While artists work from real to the abstract, architects must work from the abstract to the real.</p>
+    <Button>estimate project</Button>
+  </>
+)
+
+export default IndexPage
