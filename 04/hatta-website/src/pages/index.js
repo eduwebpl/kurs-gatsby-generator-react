51844dad7a13787b59e4351a05dd395b6de50af7
Adam Romański <adam.romanski@netguru.pl>
2019-12-22 22:49:26 +0100

    04-finish

--- 04/hatta-website/src/pages/index.js
+++ 04/hatta-website/src/pages/index.js
@@ -1,15 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import styled from 'styled-components';
 import Navigation from "../components/Navigation/Navigation";
 import Button from "../components/Button/Button";
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <>
     <h1>Hello Hatta</h1>
     <p>While artists work from real to the abstract, architects must work from the abstract to the real.</p>
     <Button>estimate project</Button>
+    <img src={data.file.publicURL} />
   </>
 )
+
+export const query = graphql`
+  {
+    file(name: {eq: "hero"}) {
+      publicURL
+    }
+  }
+`
 
 export default IndexPage
