6c8afb0e1cbba28d59fe65edd955e15ad57b84e2
Adam Romański <adam.romanski@netguru.pl>
2019-12-26 22:14:10 +0100

    05.5-finish

--- 05/hatta-website/gatsby-node.js
+++ 05/hatta-website/gatsby-node.js
@@ -1,7 +1,35 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/layouts/post.js`);
+  const result = await graphql(`
+    query queryArticles {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 700, maxHeight: 500) {
+                  src
+                }
+              }
+            }
+          }
+          excerpt(pruneLength: 50)
+        }
+      }
+    }
+  `);
+
+  result.data.allMdx.nodes.forEach(post => {
+    createPage({
+      path: `${post.frontmatter.slug}`,
+      component: blogPostTemplate,
+      context: {},
+    });
+  });
+};
