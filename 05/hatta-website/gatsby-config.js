7e696ad9d5c4da7af18939d628d04ffec52d818a
Adam Romański <adam.romanski@netguru.pl>
2019-12-26 20:35:11 +0100

    05.3-finish

--- 05/hatta-website/gatsby-config.js
+++ 05/hatta-website/gatsby-config.js
@@ -26,6 +26,13 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/src/data/articles`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-mdx`,
--- 05/hatta-website/src/pages/articles.js
+++ 05/hatta-website/src/pages/articles.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const ArticlesPage = () => (
+const ArticlesPage = ({ data }) => (
   <>
-    <h1>Blog page</h1>
-    <p>Welcome to my blog</p>
+    <h1>Articles</h1>
+    {data.allMdx.nodes.map(item => (
+      <>
+        <h2>{item.frontmatter.title}</h2>
+        <p>{item.frontmatter.author}</p>
+        <p>{item.excerpt}</p>
+      </>
+    ))}
   </>
 );
+
+export const query = graphql`
+  {
+    allMdx {
+      nodes {
+        frontmatter {
+          title
+          slug
+          author
+        }
+        excerpt(pruneLength: 50)
+      }
+    }
+  }
+`;
 
 export default ArticlesPage;
